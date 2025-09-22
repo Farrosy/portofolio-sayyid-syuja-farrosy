53e25e7a24dea5f42e0336b9817668fb96d4667d
Farrosy <faros1q2w@gmail.com>
2025-09-22 22:33:23 +0700

    first submit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // <-- TAMBAHKAN BARIS INI
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
